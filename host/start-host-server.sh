8e2c382e590c963483ad8cf5d9c7e5a700468dd2
Nate McDowell <nateclaudemcdowel@gmail.com>
2019-05-11 16:44:24 -0400

    nohup

--- host/start-host-server.sh
+++ host/start-host-server.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-node node_modules/react-scripts/scripts/start.js
-node ../server/server.js
-./tupelo-v0.2.0-linux-arm-7 rpc-server
-
-
+nohup node node_modules/react-scripts/scripts/start.js &
+nohup node ../server/server.js &
+nohup ./tupelo-v0.2.0-linux-arm-7 rpc-server &
